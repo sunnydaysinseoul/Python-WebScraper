9f447434518cab221ba4f435522ab0ca606c23fd
Youngsun <youngsunhan.kr@gmail.com>
2021-02-15 23:51:47 +0900

    Python 기본함수, if else condition 공부

---
+++ calc.py
@@ -0,0 +1,93 @@
+# -----Code Challenge! 7가지 계산함수 만들기(user의 입력오류도 고려)
+def cal_plus(a,b):
+    try : return int(a)+int(b)
+    except : print("숫자를 입력해 주세요")
+cal_plus = cal_plus("3",4)
+print(cal_plus)
+
+def cal_minus(a,b):
+    try : return int(a)-int(b)
+    except : print("숫자를 입력해 주세요")
+cal_minus = cal_minus(1,"5")
+print(cal_minus)
+
+def cal_times(a,b):
+    try : return int(a)*int(b)
+    except : print("숫자를 입력해 주세요")
+cal_times = cal_times("3",2)
+print(cal_times)
+
+def cal_division(a,b):
+    try : return int(a)/int(b)
+    except : print("숫자를 입력해 주세요")
+cal_division = cal_division("3",2)
+print(cal_division)
+
+def cal_negation(a):
+    try : return -int(a)
+    except : print("숫자를 입력해 주세요")
+cal_negation = cal_negation('10')
+print(cal_negation)
+
+def cal_power(a,b):
+    try : return pow(int(a),int(b))
+    except : print("숫자를 입력해 주세요")
+cal_power = cal_power(5,'3')
+print(cal_power)
+
+def cal_remainder(a,b): #나머지구하기
+    try : return int(a) % int(b)
+    except : print("숫자를 입력해 주세요")
+cal_remainder = cal_remainder(5,"김치")
+print(cal_remainder)
+
+
+# 다른사람방법 (if사용)
+def change(a, b, math):
+    a = int(a)
+    b = int(b)
+    try:
+        if math == "+": #이게 True일 때 return.
+            return a + b
+        elif math == "-":
+            return a - b
+        elif math == "*":
+            return a * b
+        elif math == "/":
+            return a / b
+        elif math == "**":
+            return a ** b
+    except:
+        return "Please enter a number."
+
+
+def plus(a, b):
+    return change(a, b, "+")
+
+def mius(a, b):
+    return change(a, b, "-")
+
+def times(a, b):
+    return change(a, b, "*")
+
+def division(a, b):
+    return change(a, b, "/")
+
+def negation(a):
+    if a.isdecimal(): # a 가 숫자이면
+        a = int(a)
+        return a * -1
+    else :
+        return "Please enter a number."
+
+def power(a, b):
+    return change(a, b, "**")
+
+print(plus(4, 7))
+print(mius("4", 7))
+print(times("4", "7"))
+print(division("4", "7"))
+print(negation("4"))
+print(power("4", "7"))
+print(plus("a",4))
+print(negation("haha"))
--- main.py
+++ main.py
@@ -1,6 +1,103 @@
+# tuple (값 변경 불가)
 t_days = ('Mon','Tue','Wed','Thur','Fri')
-
 print(type(t_days))
 
+# list
 l_days = ['Mon','Tue','Wed','Thur','Fri']
 print(type(l_days))
+
+# dictionary {key:value}
+youngsun = {
+    "name" : "Youngsun",
+    "age" : 25,
+    "Korean":True,
+    "fav_food":["ttoekbokki","steak"]
+}
+print(youngsun)
+youngsun["Handsome"] = True #Boolean value 추가하기
+print(youngsun)
+
+#-----function() -행동(기능)의 반복
+#Built-in functions
+print(len("adfjerqiqjksjdfidfhgqew"))
+
+age="18"
+print(type(age))
+n_age= int(age) #str을 int로 바꾸기
+print(n_age)
+print(type(n_age))
+
+# HOW TO Define a function (Indentation is important!)
+def say_hello(who):
+    print("Hello",who)
+
+say_hello("Sunny")
+#--- 함수에서 필요한 인자만큼 꼭 입력해줘야함
+def plus(a,b):
+    print(a+b)
+
+plus(2,7)
+#---
+def minus(a,b=1): # b : default value
+    print(a-b)
+
+minus(4) # a에만 입력값이 들어가서 a-b가 계산됨
+#---
+def say_hi(name = "anonymous"):
+    print("hello",name)
+say_hi() #입력할 값이 없으면 deault value가 입력되어 실행됨.
+say_hi("Sunny")
+
+# print와 return의 비교
+def p_plus(a,b):
+    print(a+b)
+
+def r_plus(a,b):
+    return(a+b) # return은 함수를 종료시킴
+
+p_result = p_plus(2,3) # print하면 함수 호출시 값이 한번만 '출력'됨. 저장은 X, 그냥 나에게 보여지는 것.
+r_result = r_plus(2,3) # return을 주면 함수 호출시 그 값이 저장됨
+print(p_result,r_result) # 결과 : None, 5
+
+# keyword argument vs positional argument
+k_result = r_plus(b=30, a=1) # a와 b의 순서가 중요X
+p_result = r_plus(1,30) # a와 b의 순서가 중요O
+
+# format"string{function}string"
+def helllllo(name,age):
+    return f"Hello {name}, You are {age} years old."
+
+hello = helllllo(age = "25",name = "sunnysunny") # keyword argument사용 (argument가 여러개 일 때 유용)
+print(hello)
+
+# ----------Code Challenge! --> "calc.py"
+
+# if, else
+def type_plus(a,b):
+    if type(b) is int or type(b) is float:
+        return a+b
+    else:
+        return None
+
+print(type_plus(12,"7")) #b가 int나 float(소수점)이 아닐 때 -> None
+print(type_plus(12,7))
+
+# if else and or
+def age_check(age):
+    print(f"You are {age} years old.")
+    if age < 18:
+        print("You can't drink.")
+    elif age == 18 or age == 19:
+        print("Congrats! You are new to this!")
+    elif age > 20 and age <25:
+        print("Oh, You are still kinda young!")
+    else:
+        print("Enjoy your drink!")
+
+age_check(13)
+age_check(18)
+age_check(22)
+age_check(41)
+
+
+
