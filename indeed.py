a50b24ab49e8999d4e57f4a182b339dded588749
Youngsun <youngsunhan.kr@gmail.com>
2021-02-21 23:27:13 +0900

    Extracting Job Titles(position)  from Indeed

--- indeed.py
+++ indeed.py
@@ -7,11 +7,12 @@ sys.stderr = io.TextIOWrapper(sys.stderr.detach(),encoding='utf-8')
 # >pip install beautifulsoup4
 import requests
 from bs4 import BeautifulSoup
-
-INDEED_URL="https://kr.indeed.com/jobs?q=python&l=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C"
+LIMIT = 50 #한 페이지 공고 개수
+URL=f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&l=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C&limit={LIMIT}&radius=25"
+        #URL맨 뒤에 &start=50이 오면 50번째 글 부터 출력 되므로 페이지 2가 나옴. &start=100이면 페이지 3이 나오게 됨.
 
 def extract_indeed_pages():
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
     soup = BeautifulSoup(result.text, 'html.parser')
 
     pagination = soup.find("div",{"class":"pagination"})
@@ -28,4 +29,18 @@ def extract_indeed_pages():
     return max_page
     
 
-# extract_indeed_pages() #확인용
+# page수 만큼 request하는 함수
+def extract_indeed_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+            #필요한 last_page는 main.py에서 지정해 줄 것임.
+        result = requests.get(f"{URL}&start={0*LIMIT}") #URL뒤에 &start=50 이런 숫자를 붙여서 페이지마다 값 가져오기
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("div",{"class":"jobsearch-SerpJobCard"}) # list
+        # print(results)
+        for result in results: #class="jobsearch-SerpJobCard"안에서 loop
+            title = result.find("h2",{"class":"title"}).find("a")["title"] #results list의 각 값(result)에서 class="title"인 <h2>를 찾고, 거기에 포함되는 class="title"인 <a>를 찾아서 출력하기.
+            # .find("a").string  #이렇게 하면 None이 포함되어 나옴
+            print(title)
+    return jobs
+
