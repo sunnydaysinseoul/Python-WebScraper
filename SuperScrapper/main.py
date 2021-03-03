4a7a62b39cfb6f659f8f10c943e837928a21dd83
Youngsun <youngsunhan.kr@gmail.com>
2021-03-03 21:19:18 +0900

    Python - flask의 routing 연습하기

---
+++ SuperScrapper/main.py
@@ -0,0 +1,35 @@
+#가상환경 만들기 (참고: https://flask.palletsprojects.com/en/1.1.x/installation/#install-virtualenv)
+# >py -3 -m venv venv
+# >venv\Scripts\activate
+# >pip install Flask
+#파일 실행하기(현재폴더에서) : >py main.py
+from flask import Flask, render_template, request, redirect
+
+app = Flask("SuperScrapper")
+
+#----- "@" : "Decorater" : 바로아래의 함수를 찾아서 실행해준다.
+@app.route("/") #host:5000/ 주소를 불렀을때 아래 함수를 실행하겠다
+def home():
+    return render_template('home.html') # import와 함께 사용
+
+# @app.route("/contact") # == http://127.0.0.1:5000/contact
+# def phone():
+#     return "Contact me! ☎010-3333-7777"
+
+@app.route("/report") #검색할 때 올 페이지
+def report(): #사용자가 찾으려고하는 단어 (parameter=value에서 value) 가져오기
+    # print(request.args) #주소의 argument를 출력
+    # print(request.args.get('word')) #html의 form에서 지정해준 word argument를 request이용해 주소에서뽑아오기
+    word = request.args.get('word') # = user가 입력한 검색값
+    if word:
+        word = word.lower()
+    else: #주소에서 user가 입력한 word가 없을 때 home으로 redirect.
+        return redirect("/")
+    return render_template("report.html",searchingBy=word)
+    #이렇게 값(word)을 받아와서 html의 '{{searchingBy}}'자리에 대체해주는 것을 "RENDERING "렌더링이라고함
+
+@app.route("/<username>") # 여러가지 url을 관리하는 방법! ex. instagram.com/ssuny.me <<- 요거
+def name(username):
+    return f"Hello your name is {username}!"
+
+app.run(host="127.0.0.1")
---
+++ SuperScrapper/scrapper.py
@@ -0,0 +1,58 @@
+# stackoverflow job scrapping
+# step 1 : get the page
+# step 2 : make the requests
+# step 3 : extract data
+
+#>pip install requests
+#>pip install beautifulsoup4
+from warnings import resetwarnings
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://stackoverflow.com/jobs?q=python"
+
+
+def get_last_page():  # 가져와야할 총 페이지수 구하기
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    pages = soup.find("div", {"class": "s-pagination"}
+                      ).find_all("a")  # 모든 a태그를 '리스트'로 가져오기
+    # print(pages)
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)  # extract_jobs()에서 range쓰기 위해
+
+
+def extract_job(html):
+    title = html.find("a")["title"]
+    link = html.find("a")["href"]
+    company = html.find("span").get_text(strip=True)
+    location = html.find(
+        "span", {"class": "fc-black-500"}).get_text(strip=True)
+    # print(title,link,company,location)
+    return {'title': title,
+            'company': company,
+            'location': location,
+            'link': f"https://stackoverflow.com{link}"}
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        # print(page + 1) #페이지수 뽑아보기
+        print(f"Now scrapping... StackOverflow page {page+1}")
+        result = requests.get(f"{URL}&pg={page+1}")
+        # print(result.status_code) #구동확인
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "grid--cell fl1"})
+        for result in results:
+            # 각각의 div를 하나씩 돌면서 extract_job을 돌려줌. ->여기서 나오는 return값을 job변수에 저장하기.
+            job = extract_job(result)
+            # print(job)
+            jobs.append(job)  # 위에서 저장한 job변수를 jobs 리스트에 넣기.
+    return jobs
+
+
+def get_jobs(word):
+    last_page = get_last_page()  # 함수에서 return된 값을 받아와서 새로운 변수에 저장
+    jobs = extract_jobs(last_page)
+    return jobs
